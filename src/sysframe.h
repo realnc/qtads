41f3442a00c036208efb8b8873d0513f7c035214
Nikos Chantziaras <realnc@users.sourceforge.net>
2010-06-11 18:50:08 +0300

    Comments

--- src/sysframe.h
+++ src/sysframe.h
@@ -95,8 +95,16 @@ class CHtmlSysFrameQt: public QApplication, public CHtmlSysFrame {
 	fRunT3Game( const QString& fname );
 
   signals:
+	// Emmited just prior to starting a game.  The game has not started yet
+	// when this is emitted.
 	void gameStarting();
+
+	// Emmited prior to quitting a game.  The game has not quit yet when this
+	// is emitted.
 	void gameQuitting();
+
+	// Emmited after quiting a game.  The game has already quit when this is
+	// emitted.
 	void gameHasQuit();
 
   public slots:
