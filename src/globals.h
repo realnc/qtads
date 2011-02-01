9bbeebbfd7b5c6524dbb7fe57d9fd0973d40d137
Nikos Chantziaras <realnc@users.sourceforge.net>
2011-02-02 00:02:36 +0200

    Bump to 2.1.0
    This reverts commit 0aca40a74e8e36b9cf0c85643faf70a0a4ce17f9, which was a revert
    in itself.  SourceForge brought services back online, so we can release now.

--- src/globals.h
+++ src/globals.h
@@ -4,7 +4,7 @@
 
 /* QTads version string.
  */
-#define QTADS_VERSION "2.0.2 git"
+#define QTADS_VERSION "2.1.0"
 
 
 /* Works like qApp, but contains the global CHtmlSysFrameQt object instead.  If
