b42fc074c183a5c36b64103a1387c1ac8c7b1aa0
Nikos Chantziaras <realnc@users.sourceforge.net>
2011-02-11 00:46:05 +0200

    Revert "Compare family names instead of using QFont::exactMatch() to determine if fonts match"
    This reverts commit 696b7f0f1761778af826be66a8943deb1b34d735.
    
    Turns out that QFont::family() is useless in determining the font that is
    actually used, since it returns whatever was set with setFamily(), even if
    such font doesn't exist.

--- src/sysframe.cc
+++ src/sysframe.cc
@@ -406,8 +406,8 @@ CHtmlSysFrameQt::createFont( const CHtmlFontDesc* font_desc )
 				base_point_size = this->fSettings->fixedFont.pointSize();
 				matchFound = true;
 			} else {
-				newFont.setFamily(s);
-				if (newFont.family().toLower() == s) {
+				newFont.setFamily(s.toLower());
+				if (newFont.exactMatch()) {
 					matchFound = true;
 					fontName = s;
 				}
