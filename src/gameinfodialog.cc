58adee5d63d28991dc013cdd63303c885c2df7c6
Nikos Chantziaras <realnc@users.sourceforge.net>
2011-02-13 10:47:36 +0200

    Display cover art images in the "Game Information" dialog

--- src/gameinfodialog.cc
+++ src/gameinfodialog.cc
@@ -1,10 +1,17 @@
 #include "gameinfodialog.h"
 #include "ui_gameinfodialog.h"
 
-#include <QMessageBox>
 #include <QDebug>
+#include <QMessageBox>
+#include <QFileInfo>
 
 #include "gameinfo.h"
+#include "globals.h"
+#include "settings.h"
+#include "sysframe.h"
+#include "syswininput.h"
+#include "htmlrf.h"
+#include "htmlfmt.h"
 
 
 /* Implementation of the game information enumerator callback interface.
@@ -39,15 +46,15 @@ class QTadsGameInfoEnum: public CTadsGameInfo_enum
 		const QString& nameStr = QString::fromUtf8(name).toLower();
 		if (nameStr == QString::fromAscii("name")) {
 			this->gameName = QString::fromAscii("<b><center><font size=\"+1\">") + Qt::escape(valStr)
-							 + QString::fromAscii("</font></center></b>");
+							 + QString::fromAscii("</font></center></b><p>");
 		} else if (nameStr == QString::fromAscii("headline")) {
 			this->headline = QString::fromAscii("<center>") + Qt::escape(valStr)
 							 + QString::fromAscii("</center><p>");
 		} else if (nameStr == QString::fromAscii("byline")) {
 			this->byLine = QString::fromAscii("<i><center>") + Qt::escape(valStr)
-						   + QString::fromAscii("</center></i>");
+						   + QString::fromAscii("</center></i><p>");
 		} else if (nameStr == QString::fromAscii("htmlbyline")) {
-			this->htmlByLine = QString::fromAscii("<i><center>") + valStr + QString::fromAscii("</center></i>");
+			this->htmlByLine = QString::fromAscii("<i><center>") + valStr + QString::fromAscii("</center></i><p>");
 		} else if (nameStr == QString::fromAscii("authoremail")) {
 			this->email = valStr;
 		} else if (nameStr == QString::fromAscii("desc")) {
@@ -94,6 +101,80 @@ insertTableRow( QTableWidget* table, const QString& text1, const QString& text2 
 }
 
 
+static QImage
+loadCoverArtImage()
+{
+	CHtmlResFinder* resFinder = qFrame->gameWindow()->get_formatter()->get_res_finder();
+
+	// Look for a cover art resource. We try four different resource names.
+	// The first two (PNG and JPG with a ".system/" prefix) are defined in the
+	// current cover art standard.  The other two were defined in the older
+	// standard which did not use a prefix.
+	QByteArray coverArtResName;
+	bool coverArtFound = false;
+	const char coverArtPaths[][21] = {".system/CoverArt.png", ".system/CoverArt.jpg",
+									  "CoverArt.png", "CoverArt.jpg"};
+	for (int i = 0; i < 4 and not coverArtFound; ++i) {
+		coverArtResName = coverArtPaths[i];
+		if (resFinder->resfile_exists(coverArtResName.constData(), coverArtResName.length())) {
+			coverArtFound = true;
+		}
+	}
+
+	if (not coverArtFound) {
+		return QImage();
+	}
+
+	CStringBuf strBuf;
+	unsigned long offset;
+	unsigned long size;
+	resFinder->get_file_info(&strBuf, coverArtResName.constData(), coverArtResName.length(),
+							 &offset, &size);
+
+	// Check if the file exists and is readable.
+	QFileInfo inf(QString::fromLocal8Bit(strBuf.get()));
+	if (not inf.exists() or not inf.isReadable()) {
+		qWarning() << "ERROR:" << inf.filePath() << "doesn't exist or is unreadable";
+		return QImage();
+	}
+
+	// Open the file and seek to the specified position.
+	QFile file(inf.filePath());
+	if (not file.open(QIODevice::ReadOnly)) {
+		qWarning() << "ERROR: Can't open file" << inf.filePath();
+		return QImage();
+	}
+	if (not file.seek(offset)) {
+		qWarning() << "ERROR: Can't seek in file" << inf.filePath();
+		file.close();
+		return QImage();
+	}
+
+	// Load the image data.
+	const QByteArray& data(file.read(size));
+	file.close();
+	if (data.isEmpty() or static_cast<unsigned long>(data.size()) < size) {
+		qWarning() << "ERROR: Could not read" << size << "bytes from file" << inf.filePath();
+		return QImage();
+	}
+	QImage image;
+	if (not image.loadFromData(data)) {
+		qWarning() << "ERROR: Could not parse image data";
+		return QImage();
+	}
+
+	// If we got here, all went well.  Return the image scaled to a
+	// 200 pixels width if it's too large.  Otherwise, return it as-is.
+	if (image.width() > 200) {
+		Qt::TransformationMode mode = qFrame->settings()->useSmoothScaling ?
+									  Qt::SmoothTransformation : Qt::FastTransformation;
+		return image.scaledToWidth(200, mode);
+	} else {
+		return image;
+	}
+}
+
+
 GameInfoDialog::GameInfoDialog( const QByteArray& fname, QWidget* parent )
   : QDialog(parent, Qt::WindowTitleHint | Qt::WindowSystemMenuHint), ui(new Ui::GameInfoDialog)
 {
@@ -102,14 +183,25 @@ GameInfoDialog::GameInfoDialog( const QByteArray& fname, QWidget* parent )
 	CTadsGameInfo info;
 	QTadsGameInfoEnum cb;
 
+	// Try to load the cover art.  If there is one, insert it into the text
+	// browser as the "CoverArt" resource.
+	const QImage& image = loadCoverArtImage();
+	if (not image.isNull()) {
+		ui->description->document()->addResource(QTextDocument::ImageResource,
+												 QUrl(QString::fromAscii("CoverArt")), image);
+		this->resize(this->width(), this->height() + image.height());
+	}
+
 	info.read_from_file(fname.constData());
 	info.enum_values(&cb);
 
 	// Fill out the description.
-	QString tmp(cb.gameName + QString::fromAscii("<p>")
-				+ cb.headline
-				+ (cb.htmlByLine.isEmpty() ? cb.byLine : cb.htmlByLine) + QString::fromAscii("<p>"));
-	tmp += cb.htmlDesc.isEmpty() ? cb.desc : cb.htmlDesc;
+	QString tmp;
+	if (not image.isNull()) {
+		tmp += QString::fromAscii("<center><img src=\"CoverArt\"></center><p>");
+	}
+	tmp += cb.gameName + cb.headline + (cb.htmlByLine.isEmpty() ? cb.byLine : cb.htmlByLine)
+		   + (cb.htmlDesc.isEmpty() ? cb.desc : cb.htmlDesc);
 	ui->description->setHtml(tmp);
 
 	// Fill out the table.
@@ -165,6 +257,10 @@ GameInfoDialog::GameInfoDialog( const QByteArray& fname, QWidget* parent )
 	ui->table->resizeColumnsToContents();
 	ui->table->resizeRowsToContents();
 	ui->table->setShowGrid(false);
+	// There's no point in having the tableview widget be any higher than the
+	// sum of all rows.  This will also make sure that the widget is not shown
+	// at all in case we have zero rows.
+	ui->table->setMaximumHeight((ui->table->rowCount() + 1) * ui->table->rowHeight(0));
 }
 
 
