2fcbacb5ce759237015e5e69de84e380b137e8d5
Nikos Chantziaras <realnc@users.sourceforge.net>
2012-07-31 15:44:38 +0300

    Also handle hidden and system files in the os_open_dir handle

--- src/osqt.cc
+++ src/osqt.cc
@@ -261,7 +261,7 @@ int
 os_open_dir( const char* dirname, osdirhdl_t* handle )
 {
     QDirIterator* d = new QDirIterator(QString::fromLocal8Bit(dirname),
-                                       QDir::Dirs | QDir::Files);
+                                       QDir::Dirs | QDir::Files | QDir::Hidden | QDir::System);
     *handle = d;
     return true;
 }
