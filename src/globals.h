1da338e52fb7dfe4daa57ccc9a47641e7943d643
Nikos Chantziaras <realnc@users.sourceforge.net>
2013-01-02 20:02:52 +0200

    Mark version as "git"

--- src/globals.h
+++ src/globals.h
@@ -2,11 +2,11 @@
 #define GLOBALS_H
 
 /* QTads version string. */
-#define QTADS_VERSION "2.1.6"
+#define QTADS_VERSION "2.1.6 git"
 
 /* MS Windows executable resource versions. */
-#define W32_RC_FILEVERSION 2,1,6,00
-#define W32_RC_PRODUCTVERSION 2,1,6,00
+#define W32_RC_FILEVERSION 2,1,6,99
+#define W32_RC_PRODUCTVERSION 2,1,6,99
 
 #ifdef __cplusplus
 /* Works like qApp, but contains the global CHtmlSysFrameQt object instead.  If
