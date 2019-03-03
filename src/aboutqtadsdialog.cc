88e20d98ca0ad9af9644717a0de1b8bb5d8f14f2
Nikos Chantziaras <realnc@gmail.com>
2019-03-03 19:56:38 +0200

    Simplify the "about" dialog

--- src/aboutqtadsdialog.cc
+++ src/aboutqtadsdialog.cc
@@ -21,23 +21,41 @@ AboutQtadsDialog::AboutQtadsDialog(QWidget* parent)
     ui->buttonBox->setStandardButtons(QDialogButtonBox::Close);
 #endif
 
+    ui->aboutLabel->setText(tr(R"(
+        <h1>QTads</h1>
+        <br>%1
+        <p><a href="https://realnc.github.io/qtads">https://realnc.github.io/qtads</a></p>
+
+        <p>
+        TADS Copyright © 2013 Michael J. Roberts<br>
+        QTads Copyright © 2019 Nikos Chantziaras
+        </p>
+
+        <p>
+        QTads is free software: you can redistribute it and/or modify it under the terms of the
+        <a href="https://realnc.github.io/qtads/gpl-3.0-standalone.html">GNU General Public
+        License</a> as published by the Free Software Foundation, either version 3 of the License,
+        or (at your option) any later version.
+        </p>
+    )")
+                                .arg(QLatin1String(QTADS_VERSION)));
+
     // Construct a string holding all version info.
     QString str;
-    str += QString::fromLatin1("<table border=\"0\" width=\"100%\"><tr><td>");
-    str += tr("QTads version:") + QString::fromLatin1("</td><td>")
-           + QString::fromLatin1(QTADS_VERSION) + QString::fromLatin1("<br></td></tr><tr><td>")
-           + tr("HTML TADS version:") + QString::fromLatin1("</td><td>\t")
-           + QString::fromLatin1(HTMLTADS_VERSION) + QString::fromLatin1("</td></tr><tr><td>")
-           + tr("TADS 2 virtual machine:") + QString::fromLatin1("</td><td>\t")
-           + QString::fromLatin1(TADS_RUNTIME_VERSION) + QString::fromLatin1("</td></tr><tr><td>")
-           + tr("TADS 3 virtual machine:") + QString::fromLatin1("</td><td>\t")
-           + QString::fromLatin1(T3VM_VSN_STRING) + QString::fromLatin1(" (")
-           + QString::fromLatin1(T3VM_IDENTIFICATION)
+    str += QString::fromLatin1("<p><table border=\"0\" width=\"100%\"><tr><td>");
+    str += tr("QTads:") + QString::fromLatin1("</td><td>") + QString::fromLatin1(QTADS_VERSION)
+           + QString::fromLatin1("<br></td></tr><tr><td>") + tr("HTML TADS:")
+           + QString::fromLatin1("</td><td>\t") + QString::fromLatin1(HTMLTADS_VERSION)
+           + QString::fromLatin1("</td></tr><tr><td>") + tr("TADS 2 virtual machine:")
+           + QString::fromLatin1("</td><td>\t") + QString::fromLatin1(TADS_RUNTIME_VERSION)
+           + QString::fromLatin1("</td></tr><tr><td>") + tr("TADS 3 virtual machine:")
+           + QString::fromLatin1("</td><td>\t") + QString::fromLatin1(T3VM_VSN_STRING)
+           + QString::fromLatin1(" (") + QString::fromLatin1(T3VM_IDENTIFICATION)
            + QString::fromLatin1(")<br></td></tr><tr><td>") + tr("Qt build version:")
            + QString::fromLatin1("</td><td>") + QString::fromLatin1(QT_VERSION_STR)
            + QString::fromLatin1("</td></tr><tr><td>") + tr("Qt runtime version:")
            + QString::fromLatin1("</td><td>") + QString::fromLatin1(qVersion())
-           + QString::fromLatin1("</td></tr></table>");
+           + QString::fromLatin1("</td></tr></table></p>");
     ui->versionInfoLabel->setText(str);
 }
 
