43289a830a18c66a293c2b1ee75a08e92e8dd5dc
Nikos Chantziaras <realnc@gmail.com>
2019-04-12 23:05:27 +0300

    Sync KColorButton with KWidgetsAddons 5.56.0

--- src/kcolorbutton.cc
+++ src/kcolorbutton.cc
@@ -20,53 +20,120 @@
 
 #include "kcolorbutton.h"
 
-#include <QApplication>
-#include <QClipboard>
-#include <QColorDialog>
-#include <QDrag>
-#include <QMimeData>
-#include <QMouseEvent>
+#include <QPointer>
 #include <QPainter>
-#include <QStyle>
-#include <QStyleOptionButton>
 #include <qdrawutil.h>
+#include <QApplication>
+#include <QColorDialog>
+#include <QClipboard>
+#include <QMimeData>
+#include <QDrag>
+#include <QStyle>
+#include <QMouseEvent>
+#include <QStyleOptionButton>
 
-KColorButton::KColorButton(QWidget* parent)
+class Q_DECL_HIDDEN KColorButton::KColorButtonPrivate
+{
+public:
+    KColorButtonPrivate(KColorButton *q);
+
+    void _k_chooseColor();
+    void _k_colorChosen();
+
+    KColorButton *q;
+    QColor m_defaultColor;
+    bool m_bdefaultColor : 1;
+    bool m_alphaChannel : 1;
+
+    QColor col;
+    QPoint mPos;
+
+    QPointer<QColorDialog> dialogPtr;
+
+    void initStyleOption(QStyleOptionButton *opt) const;
+};
+
+/////////////////////////////////////////////////////////////////////
+// Functions duplicated from KColorMimeData
+// Should be kept in sync
+void _k_populateMimeData(QMimeData *mimeData, const QColor &color)
+{
+    mimeData->setColorData(color);
+    mimeData->setText(color.name());
+}
+
+bool _k_canDecode(const QMimeData *mimeData)
+{
+    if (mimeData->hasColor()) {
+        return true;
+    }
+    if (mimeData->hasText()) {
+        const QString colorName = mimeData->text();
+        if ((colorName.length() >= 4) && (colorName[0] == QLatin1Char('#'))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+QColor _k_fromMimeData(const QMimeData *mimeData)
+{
+    if (mimeData->hasColor()) {
+        return mimeData->colorData().value<QColor>();
+    }
+    if (_k_canDecode(mimeData)) {
+        return QColor(mimeData->text());
+    }
+    return QColor();
+}
+
+QDrag *_k_createDrag(const QColor &color, QObject *dragsource)
+{
+    QDrag *drag = new QDrag(dragsource);
+    QMimeData *mime = new QMimeData;
+    _k_populateMimeData(mime, color);
+    drag->setMimeData(mime);
+    QPixmap colorpix(25, 20);
+    colorpix.fill(color);
+    QPainter p(&colorpix);
+    p.setPen(Qt::black);
+    p.drawRect(0, 0, 24, 19);
+    p.end();
+    drag->setPixmap(colorpix);
+    drag->setHotSpot(QPoint(-5, -7));
+    return drag;
+}
+/////////////////////////////////////////////////////////////////////
+
+KColorButton::KColorButtonPrivate::KColorButtonPrivate(KColorButton *q)
+    : q(q)
+{
+    m_bdefaultColor = false;
+    m_alphaChannel = false;
+    q->setAcceptDrops(true);
+
+    connect(q, &KColorButton::clicked, q, [this]() { _k_chooseColor(); });
+}
+
+KColorButton::KColorButton(QWidget *parent)
     : QPushButton(parent)
     , d(new KColorButtonPrivate(this))
 {
-    d->m_bdefaultColor = false;
-    d->m_defaultColor = QColor();
-    setAcceptDrops(true);
-
-    // 2000-10-15 (putzer): fixes broken keyboard usage
-    connect(this, SIGNAL(clicked()), this, SLOT(_k_chooseColor()));
 }
 
-KColorButton::KColorButton(const QColor& c, QWidget* parent)
+KColorButton::KColorButton(const QColor &c, QWidget *parent)
     : QPushButton(parent)
     , d(new KColorButtonPrivate(this))
 {
     d->col = c;
-    d->m_bdefaultColor = false;
-    d->m_defaultColor = QColor();
-    setAcceptDrops(true);
-
-    // 2000-10-15 (putzer): fixes broken keyboard usage
-    connect(this, SIGNAL(clicked()), this, SLOT(_k_chooseColor()));
 }
 
-KColorButton::KColorButton(const QColor& c, const QColor& defaultColor, QWidget* parent)
+KColorButton::KColorButton(const QColor &c, const QColor &defaultColor, QWidget *parent)
     : QPushButton(parent)
     , d(new KColorButtonPrivate(this))
 {
     d->col = c;
-    d->m_bdefaultColor = true;
-    d->m_defaultColor = defaultColor;
-    setAcceptDrops(true);
-
-    // 2000-10-15 (putzer): fixes broken keyboard usage
-    connect(this, SIGNAL(clicked()), this, SLOT(_k_chooseColor()));
+    setDefaultColor(defaultColor);
 }
 
 KColorButton::~KColorButton()
@@ -79,13 +146,23 @@ QColor KColorButton::color() const
     return d->col;
 }
 
-void KColorButton::setColor(const QColor& c)
+void KColorButton::setColor(const QColor &c)
 {
     if (d->col != c) {
         d->col = c;
-        repaint();
+        update();
         emit changed(d->col);
     }
+}
+
+void KColorButton::setAlphaChannelEnabled(bool alpha)
+{
+    d->m_alphaChannel = alpha;
+}
+
+bool KColorButton::isAlphaChannelEnabled() const
+{
+    return d->m_alphaChannel;
 }
 
 QColor KColorButton::defaultColor() const
@@ -93,57 +170,72 @@ QColor KColorButton::defaultColor() const
     return d->m_defaultColor;
 }
 
-void KColorButton::setDefaultColor(const QColor& c)
+void KColorButton::setDefaultColor(const QColor &c)
 {
     d->m_bdefaultColor = c.isValid();
     d->m_defaultColor = c;
 }
 
-void KColorButton::KColorButtonPrivate::initStyleOption(QStyleOptionButton* opt) const
+void KColorButton::KColorButtonPrivate::initStyleOption(QStyleOptionButton *opt) const
 {
     opt->initFrom(q);
     opt->state |= q->isDown() ? QStyle::State_Sunken : QStyle::State_Raised;
     opt->features = QStyleOptionButton::None;
-    if (q->isDefault())
+    if (q->isDefault()) {
         opt->features |= QStyleOptionButton::DefaultButton;
+    }
     opt->text.clear();
     opt->icon = QIcon();
 }
 
-void KColorButton::paintEvent(QPaintEvent*)
+void KColorButton::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
+    QStyle *style = QWidget::style();
 
-    // First, we need to draw the bevel.
+    //First, we need to draw the bevel.
     QStyleOptionButton butOpt;
     d->initStyleOption(&butOpt);
-    style()->drawControl(QStyle::CE_PushButtonBevel, &butOpt, &painter, this);
+    style->drawControl(QStyle::CE_PushButtonBevel, &butOpt, &painter, this);
 
-    // OK, now we can muck around with drawing out pretty little color box
-    // First, sort out where it goes
-    QRect labelRect = style()->subElementRect(QStyle::SE_PushButtonContents, &butOpt, this);
-    int shift = style()->pixelMetric(QStyle::PM_ButtonMargin) / 2;
+    //OK, now we can muck around with drawing out pretty little color box
+    //First, sort out where it goes
+    QRect labelRect = style->subElementRect(QStyle::SE_PushButtonContents,
+                                            &butOpt, this);
+    int shift = style->pixelMetric(QStyle::PM_ButtonMargin, &butOpt, this) / 2;
     labelRect.adjust(shift, shift, -shift, -shift);
     int x, y, w, h;
     labelRect.getRect(&x, &y, &w, &h);
 
     if (isChecked() || isDown()) {
-        x += style()->pixelMetric(QStyle::PM_ButtonShiftHorizontal);
-        y += style()->pixelMetric(QStyle::PM_ButtonShiftVertical);
+        x += style->pixelMetric(QStyle::PM_ButtonShiftHorizontal, &butOpt, this);
+        y += style->pixelMetric(QStyle::PM_ButtonShiftVertical, &butOpt, this);
     }
 
     QColor fillCol = isEnabled() ? d->col : palette().color(backgroundRole());
-    qDrawShadePanel(&painter, x, y, w, h, palette(), true, 1, NULL);
-    if (fillCol.isValid())
-        painter.fillRect(x + 1, y + 1, w - 2, h - 2, fillCol);
+    qDrawShadePanel(&painter, x, y, w, h, palette(), true, 1, nullptr);
+    if (fillCol.isValid()) {
+        const QRect rect(x + 1, y + 1, w - 2, h - 2);
+        if (fillCol.alpha() < 255) {
+            QPixmap chessboardPattern(16, 16);
+            QPainter patternPainter(&chessboardPattern);
+            patternPainter.fillRect(0, 0, 8, 8, Qt::black);
+            patternPainter.fillRect(8, 8, 8, 8, Qt::black);
+            patternPainter.fillRect(0, 8, 8, 8, Qt::white);
+            patternPainter.fillRect(8, 0, 8, 8, Qt::white);
+            patternPainter.end();
+            painter.fillRect(rect, QBrush(chessboardPattern));
+        }
+        painter.fillRect(rect, fillCol);
+    }
 
     if (hasFocus()) {
-        QRect focusRect = style()->subElementRect(QStyle::SE_PushButtonFocusRect, &butOpt, this);
+        QRect focusRect = style->subElementRect(QStyle::SE_PushButtonFocusRect, &butOpt, this);
         QStyleOptionFocusRect focusOpt;
         focusOpt.init(this);
-        focusOpt.rect = focusRect;
+        focusOpt.rect            = focusRect;
         focusOpt.backgroundColor = palette().background().color();
-        style()->drawPrimitive(QStyle::PE_FrameFocusRect, &focusOpt, &painter, this);
+        style->drawPrimitive(QStyle::PE_FrameFocusRect, &focusOpt, &painter, this);
     }
 }
 
@@ -151,82 +243,93 @@ QSize KColorButton::sizeHint() const
 {
     QStyleOptionButton opt;
     d->initStyleOption(&opt);
-    return style()
-        ->sizeFromContents(QStyle::CT_PushButton, &opt, QSize(40, 15), this)
-        .expandedTo(QApplication::globalStrut());
+    return style()->sizeFromContents(QStyle::CT_PushButton, &opt, QSize(40, 15), this).
+           expandedTo(QApplication::globalStrut());
 }
 
 QSize KColorButton::minimumSizeHint() const
 {
     QStyleOptionButton opt;
     d->initStyleOption(&opt);
-    return style()
-        ->sizeFromContents(QStyle::CT_PushButton, &opt, QSize(3, 3), this)
-        .expandedTo(QApplication::globalStrut());
+    return style()->sizeFromContents(QStyle::CT_PushButton, &opt, QSize(3, 3), this).
+           expandedTo(QApplication::globalStrut());
 }
 
-void KColorButton::dragEnterEvent(QDragEnterEvent* event)
+void KColorButton::dragEnterEvent(QDragEnterEvent *event)
 {
-    event->setAccepted(qvariant_cast<QColor>(event->mimeData()->colorData()).isValid()
-                       && isEnabled());
+    event->setAccepted(_k_canDecode(event->mimeData()) && isEnabled());
 }
 
-void KColorButton::dropEvent(QDropEvent* event)
+void KColorButton::dropEvent(QDropEvent *event)
 {
-    const QColor& c = qvariant_cast<QColor>(event->mimeData()->colorData());
+    QColor c = _k_fromMimeData(event->mimeData());
     if (c.isValid()) {
         setColor(c);
     }
 }
 
-void KColorButton::keyPressEvent(QKeyEvent* e)
+void KColorButton::keyPressEvent(QKeyEvent *e)
 {
-    if (e->matches(QKeySequence::Copy)) {
-        QMimeData* mime = new QMimeData;
-        mime->setColorData(color());
+    int key = e->key() | e->modifiers();
+
+    if (QKeySequence::keyBindings(QKeySequence::Copy).contains(key)) {
+        QMimeData *mime = new QMimeData;
+        _k_populateMimeData(mime, color());
         QApplication::clipboard()->setMimeData(mime, QClipboard::Clipboard);
-    } else if (e->matches(QKeySequence::Paste)) {
-        const QColor& color = qvariant_cast<QColor>(
-            QApplication::clipboard()->mimeData(QClipboard::Clipboard)->colorData());
+    } else if (QKeySequence::keyBindings(QKeySequence::Paste).contains(key)) {
+        QColor color = _k_fromMimeData(QApplication::clipboard()->mimeData(QClipboard::Clipboard));
         setColor(color);
-    } else
+    } else {
         QPushButton::keyPressEvent(e);
+    }
 }
 
-void KColorButton::mousePressEvent(QMouseEvent* e)
+void KColorButton::mousePressEvent(QMouseEvent *e)
 {
     d->mPos = e->pos();
     QPushButton::mousePressEvent(e);
 }
 
-void KColorButton::mouseMoveEvent(QMouseEvent* e)
+void KColorButton::mouseMoveEvent(QMouseEvent *e)
 {
-    QPushButton::mouseMoveEvent(e);
-    if ((e->buttons() & Qt::LeftButton)
-        && (e->pos() - d->mPos).manhattanLength() > QApplication::startDragDistance()) {
-        QDrag* drag = new QDrag(this);
-        QMimeData* mime = new QMimeData;
-        mime->setColorData(color());
-        drag->setMimeData(mime);
-        drag->exec(Qt::CopyAction);
+    if ((e->buttons() & Qt::LeftButton) &&
+            (e->pos() - d->mPos).manhattanLength() > QApplication::startDragDistance()) {
+        _k_createDrag(color(), this)->start();
         setDown(false);
     }
 }
 
 void KColorButton::KColorButtonPrivate::_k_chooseColor()
 {
-    QColor c = q->color();
-    if (m_bdefaultColor) {
-        c = QColorDialog::getColor(m_defaultColor, q);
-        if (c.isValid()) {
-            q->setColor(c);
-        } else {
-            q->setColor(m_defaultColor);
-        }
-    } else {
-        c = QColorDialog::getColor(c, q);
-        if (c.isValid()) {
-            q->setColor(c);
-        }
+    QColorDialog *dialog = dialogPtr.data();
+    if (dialog) {
+        dialog->show();
+        dialog->raise();
+        dialog->activateWindow();
+        return;
+    }
+
+    dialog = new QColorDialog(q);
+    dialog->setCurrentColor(q->color());
+    dialog->setOption(QColorDialog::ShowAlphaChannel, m_alphaChannel);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    connect(dialog, SIGNAL(accepted()), q, SLOT(_k_colorChosen()));
+    dialogPtr = dialog;
+    dialog->show();
+}
+
+void KColorButton::KColorButtonPrivate::_k_colorChosen()
+{
+    QColorDialog *dialog = dialogPtr.data();
+    if (!dialog) {
+        return;
+    }
+
+    if (dialog->selectedColor().isValid()) {
+        q->setColor(dialog->selectedColor());
+    } else if (m_bdefaultColor) {
+        q->setColor(m_defaultColor);
     }
 }
+
+#include "moc_kcolorbutton.cpp"
--- src/kcolorbutton.h
+++ src/kcolorbutton.h
@@ -22,39 +22,41 @@
 
 #include <QPushButton>
 
-class KColorButtonPrivate;
 /**
- * @short A pushbutton to display or allow user selection of a color.
- *
- * This widget can be used to display or allow user selection of a color.
- *
- * @see KColorDialog
- *
- * \image html kcolorbutton.png "KDE Color Button"
- */
-class KColorButton: public QPushButton
+* @class KColorButton kcolorbutton.h KColorButton
+*
+* @short A pushbutton to display or allow user selection of a color.
+*
+* This widget can be used to display or allow user selection of a color.
+*
+* \image html kcolorbutton.png "KColorButton Widget"
+*
+* @see QColorDialog
+*/
+class KColorButton : public QPushButton
 {
     Q_OBJECT
-    Q_PROPERTY(QColor color READ color WRITE setColor USER true)
+    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY changed USER true)
     Q_PROPERTY(QColor defaultColor READ defaultColor WRITE setDefaultColor)
+    Q_PROPERTY(bool alphaChannelEnabled READ isAlphaChannelEnabled WRITE setAlphaChannelEnabled)
 
 public:
     /**
      * Creates a color button.
      */
-    explicit KColorButton(QWidget* parent = 0);
+    explicit KColorButton(QWidget *parent = nullptr);
 
     /**
      * Creates a color button with an initial color @p c.
      */
-    explicit KColorButton(const QColor& c, QWidget* parent = 0);
+    explicit KColorButton(const QColor &c, QWidget *parent = nullptr);
 
     /**
      * Creates a color button with an initial color @p c and default color @p defaultColor.
      */
-    KColorButton(const QColor& c, const QColor& defaultColor, QWidget* parent = 0);
+    KColorButton(const QColor &c, const QColor &defaultColor, QWidget *parent = nullptr);
 
-    virtual ~KColorButton();
+    ~KColorButton() override;
 
     /**
      * Returns the currently chosen color.
@@ -64,7 +66,20 @@ public:
     /**
      * Sets the current color to @p c.
      */
-    void setColor(const QColor& c);
+    void setColor(const QColor &c);
+
+    /**
+     * When set to true, allow the user to change the alpha component
+     * of the color. The default value is false.
+     * @since 4.5
+     */
+    void setAlphaChannelEnabled(bool alpha);
+
+    /**
+     * Returns true if the user is allowed to change the alpha component.
+     * @since 4.5
+     */
+    bool isAlphaChannelEnabled() const;
 
     /**
      * Returns the default color or an invalid color
@@ -75,50 +90,30 @@ public:
     /**
      * Sets the default color to @p c.
      */
-    void setDefaultColor(const QColor& c);
+    void setDefaultColor(const QColor &c);
 
-    QSize sizeHint() const;
-    QSize minimumSizeHint() const;
+    QSize sizeHint() const override;
+    QSize minimumSizeHint() const override;
 
 Q_SIGNALS:
     /**
      * Emitted when the color of the widget
      * is changed, either with setColor() or via user selection.
      */
-    void changed(const QColor& newColor);
+    void changed(const QColor &newColor);
 
 protected:
-    virtual void paintEvent(QPaintEvent* pe);
-    virtual void dragEnterEvent(QDragEnterEvent*);
-    virtual void dropEvent(QDropEvent*);
-    virtual void mousePressEvent(QMouseEvent* e);
-    virtual void mouseMoveEvent(QMouseEvent* e);
-    virtual void keyPressEvent(QKeyEvent* e);
+    void paintEvent(QPaintEvent *pe) override;
+    void dragEnterEvent(QDragEnterEvent *) override;
+    void dropEvent(QDropEvent *) override;
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseMoveEvent(QMouseEvent *e) override;
+    void keyPressEvent(QKeyEvent *e) override;
 
 private:
-    class KColorButtonPrivate
-    {
-    public:
-        KColorButtonPrivate(KColorButton* q)
-            : q(q)
-        {}
-
-        void _k_chooseColor();
-
-        KColorButton* q;
-        QColor m_defaultColor;
-        bool m_bdefaultColor : 1;
-
-        bool dragFlag : 1;
-        QColor col;
-        QPoint mPos;
-
-        void initStyleOption(QStyleOptionButton* opt) const;
-    };
-
-    KColorButtonPrivate* const d;
-
-    Q_PRIVATE_SLOT(d, void _k_chooseColor())
+    class KColorButtonPrivate;
+    KColorButtonPrivate *const d;
+    Q_PRIVATE_SLOT(d, void _k_colorChosen())
 };
 
 #endif
